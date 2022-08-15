c438bc1d8f2e0684bd46bc0d73cc1b2de3bdbff2
MauroMedina <M.Xtreme123@Gmail.com>
2022-08-15 14:18:34 -0300

    ComponentesYPracticaImagenLogo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BannerComponent } from './Header/banner/banner.component';
+import { AcercaDeComponent } from './Header/acerca-de/acerca-de.component';
+import { ExperienciaLaboralComponent } from './experiencia-laboral/experiencia-laboral.component';
+import { EstudiosComponent } from './estudios/estudios.component';
+import { HardAndSoftSkillsComponent } from './hard-and-soft-skills/hard-and-soft-skills.component';
+import { ProyectosComponent } from './proyectos/proyectos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent,
+    AcercaDeComponent,
+    ExperienciaLaboralComponent,
+    EstudiosComponent,
+    HardAndSoftSkillsComponent,
+    ProyectosComponent
   ],
   imports: [
     BrowserModule,
